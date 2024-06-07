41d5a3abdf9896d866896707754b698dacbaaaef
Rohid <rohidul209@gmail.com>
2024-06-07 23:21:01 +0600

    cleanup

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,23 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontFamily: {
-        sans: ["var(--font-geist-sans)"],
-        mono: ["var(--font-geist-mono)"],
-      },
-    },
-  },
-  plugins: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
 };
 export default config;
