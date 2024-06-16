d35f682e1554f00adcbb76a5b1813805ae39f260
Rohid <rohidul209@gmail.com>
2024-06-17 00:02:19 +0600

    postgres

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,11 +1,11 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
 
 export * from "drizzle-orm";
 export { alias } from "drizzle-orm/pg-core";
 
-const sql = neon(process.env.DATABASE_URL!);
+const client = postgres(process.env.DATABASE_URL!);
 
 export type DB = ReturnType<typeof drizzle<typeof schema>>;
 
@@ -14,7 +14,7 @@ const globalForDb = globalThis as unknown as {
   db: DB | undefined;
 };
 
-export const db = globalForDb.db ?? drizzle(sql, { schema });
+export const db = globalForDb.db ?? drizzle(client, { schema });
 
 if (process.env.NODE_ENV !== "production") {
   globalForDb.db = db;
